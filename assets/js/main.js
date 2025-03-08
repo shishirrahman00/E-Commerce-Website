cc2830c95738ed610589f5075b5c95377634a166
Shishir Rahman Shuvo <shishirrahman23@gmail.com>
2025-03-08 16:46:55 +0600

    Added slider to product cart

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+const mySlider = document.querySelector(".my-slider");
+const Slider = document.querySelector(".slider");
+const testimonialSlider = document.querySelector(".testimonial-slider");
+
+if (mySlider) {
+  tns({
+    container: mySlider,
+    slideBy: "page",
+    mouseDrag: true,
+    controls: false,
+    gutter: 25,
+    responsive: {
+      425: {
+        items: 1.6,
+        gutter: 20,
+      },
+      768: {
+        items: 2.6,
+      },
+      1024: {
+        items: 3.6,
+      },
+      1440: {
+        items: 4,
+      },
+    },
+  });
+}
+
+if (Slider) {
+  tns({
+    container: Slider,
+    slideBy: "page",
+    mouseDrag: true,
+    controls: false,
+    gutter: 25,
+    responsive: {
+      425: {
+        items: 1.6,
+        gutter: 20,
+      },
+      768: {
+        items: 2.6,
+      },
+      1024: {
+        items: 3.6,
+      },
+      1440: {
+        items: 4,
+      },
+    },
+  });
+}
+if (testimonialSlider) {
+  tns({
+    container: testimonialSlider,
+    slideBy: "page",
+    controls: true,
+    // controlsContainer: "#controls",
+    // prevButton: ".previous",
+    // nextButton: ".next",
+    mouseDrag: true,
+    gutter: 25,
+    responsive: {
+      425: {
+        items: 1,
+        gutter: 20,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+      },
+      1440: {
+        items: 3,
+      },
+    },
+  });
+}

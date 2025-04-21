99f4298af55f0ccb84e2cdde536985eec608afb0
shishirrahman00 <shishirrahman23@gmail.com>
2025-04-21 18:00:40 +0600

    fixed slider custom arrows to our customer section in index.html

--- assets/js/cart-page.js
+++ assets/js/cart-page.js
@@ -9,92 +9,6 @@ offerBtnClose.addEventListener('click', () => {
 
 // --------------------OFFER SECTION END----------------
 
-const Slider = document.querySelector('.slider');
-const testimonialSlider = document.querySelector('.testimonial-slider');
-
-// SLIDER SECTION START
-
-const mySlider = document.querySelector('.my-slider');
-
-if (mySlider) {
-  tns({
-    container: mySlider,
-    slideBy: 'page',
-    mouseDrag: true,
-    controls: false,
-    gutter: 25,
-    responsive: {
-      425: {
-        items: 1.6,
-        gutter: 20,
-      },
-      768: {
-        items: 2.6,
-      },
-      1024: {
-        items: 3.6,
-      },
-      1440: {
-        items: 4,
-      },
-    },
-  });
-}
-
-// SLIDER SECTION END
-
-if (Slider) {
-  tns({
-    container: Slider,
-    slideBy: 'page',
-    mouseDrag: true,
-    controls: false,
-    gutter: 25,
-    responsive: {
-      425: {
-        items: 1.6,
-        gutter: 20,
-      },
-      768: {
-        items: 2.6,
-      },
-      1024: {
-        items: 3.6,
-      },
-      1440: {
-        items: 4,
-      },
-    },
-  });
-}
-if (testimonialSlider) {
-  tns({
-    container: testimonialSlider,
-    slideBy: 'page',
-    slideBy: 1,
-    mouseDrag: true,
-    controlsContainer: '#customControlsContainer',
-    prevButton: '#prev',
-    nextButton: '#next',
-    gutter: 25,
-    responsive: {
-      425: {
-        items: 1,
-        gutter: 20,
-      },
-      768: {
-        items: 2,
-      },
-      1024: {
-        items: 3,
-      },
-      1440: {
-        items: 3,
-      },
-    },
-  });
-}
-
 // -----------------------NAV MENU START
 
 const navMenuBtn = document.querySelector('.nav-menu-btn');
@@ -120,6 +34,8 @@ navMenuSliderCloseBtn.addEventListener('click', () => {
 const navSearchBtn = document.querySelector('.nav-search-btn');
 const navSearchover = document.querySelector('.search-hover');
 
+// const navSearchBtnHoverClose = document.querySelector('#nav-search-btn-hover-close');
+
 navSearchBtn.addEventListener('click', (event) => {
   navSearchover.classList.toggle('hidden');
 });
@@ -130,6 +46,45 @@ document.addEventListener('click', (event) => {
   }
 });
 
+// navSearchBtnHoverClose.addEventListener('click', () => {
+//   navSearchover.setAttribute('data-click', 'close');
+// });
+
+// const navSearchBtnHover = document.querySelector('#nav-search-btn-hover');
+
+// navSearchBtn.addEventListener('click', (event) => {
+//   const value = event.currentTarget.getAttribute('data-click');
+//   if (value === 'close') {
+//     navSearchBtnHover.setAttribute('data-click', 'open');
+//     //  navBar.classList.add("hidden")
+//   }
+// });
+
 // NAV MENU SEARCHBOX END
 
 //------------------------- NAV MENU END
+
+//----------------------INCREASE DECREASE COUNTING START
+
+const minusValue = document.querySelector('[data-value=minus]');
+const plusValue = document.querySelector('[data-value=plus]');
+const inputValue = document.querySelector('[data-value=input]');
+console.log(inputValue);
+
+let initialValue = 0;
+
+plusValue.addEventListener('click', () => {
+  initialValue++;
+  initialValue = initialValue < 10 ? '0' + initialValue : initialValue;
+  inputValue.value = initialValue;
+});
+
+minusValue.addEventListener('click', () => {
+  if (initialValue > 0) {
+    initialValue--;
+    initialValue = initialValue < 10 ? '0' + initialValue : initialValue;
+    inputValue.value = initialValue;
+  }
+});
+
+//----------------------INCREASE DECREASE COUNTING END
